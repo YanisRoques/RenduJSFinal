86164a922dd9f4c5c55784d449c3a0df6a16006b
YanisRoques <yanis.roques@latoile.em-lyon.com>
2025-05-21 16:01:05 +0200

    update

--- script.js
+++ script.js
@@ -28,9 +28,16 @@ function afficher() {
         .then(response => response.json())
         .then(data => {
             const section1 = document.getElementById("section1");
+            const s1div1 = document.createElement(`div`);
+            s1div1.id = `entete`;
+            const s1div2 = document.createElement(`div`);
+            s1div1.className = `accroche`;
+
             const section2 = document.getElementById("section2");
-            const div1 = document.createElement(`div`);
-            const div2 = document.createElement(`div`);
+            const s2div1 = document.createElement(`div`);
+            const s2div2 = document.createElement(`div`);
+            const s2div3 = document.createElement(`div`);
+
 
             let titre = document.createElement(`h1`);
             titre.textContent = data.nomCommercial;
@@ -45,35 +52,66 @@ function afficher() {
             promesses.forEach(promesse => {
                 let promessetxt = document.createElement(`p`);
                 promessetxt.textContent = promesse;
-                div2.appendChild(promessetxt);
+                s1div2.appendChild(promessetxt);
             });
 
             let plats = data.plats;
             plats.forEach(plat => {
                 let divplat = document.createElement(`div`);
-                section2.appendChild(divplat);
+                s2div1.appendChild(divplat);
 
                 let nomPlat = document.createElement(`h2`);
                 nomPlat.textContent = plat.nom;
 
                 let descPlat = document.createElement(`p`);
-                    descPlat.textContent = plat.desc;
-                
+                descPlat.textContent = plat.desc;
+
                 let imgPlat = document.createElement(`img`);
-                    imgPlat.src = plat.image-url;
-                
+                imgPlat.src = plat["image-url"];
+
                 divplat.appendChild(nomPlat);
                 divplat.appendChild(descPlat);
                 divplat.appendChild(imgPlat);
-                console.log(plat.nom);
-                console.log(plat.desc);                
             })
 
-            section1.appendChild(div1);
-            div1.appendChild(titre);
-            div1.appendChild(accroche);
-            div1.appendChild(appelAction);
-            section1.appendChild(div2);
+            let services = data.services;
+            services.forEach(service => {
+                let divservice = document.createElement(`div`);
+                s2div2.appendChild(divservice);
+
+                let nomService = document.createElement(`h2`);
+                    nomService.textContent = service.nom;
+                
+                let descService = document.createElement(`p`);
+                descService.textContent = service.desc;
+
+                divservice.appendChild(nomService);
+                divservice.appendChild(descService);
+            })
+
+            let avis = data.temoignages;
+            avis.forEach(avi => {
+                let divavis = document.createElement(`div`);
+                s2div3.appendChild(divavis);
+
+                let prenomAvi = document.createElement(`h2`);
+                prenomAvi.textContent = avi.prenom;
+
+                let comAvi = document.createElement(`p`);
+                comAvi.textContent = avi.commentaire;
+
+                divavis.appendChild(prenomAvi);
+                divavis.appendChild(comAvi);
+            })
+
+            section1.appendChild(s1div1);
+            s1div1.appendChild(titre);
+            s1div1.appendChild(accroche);
+            s1div1.appendChild(appelAction);
+            section1.appendChild(s1div2);
+            section2.appendChild(s2div1);
+            section2.appendChild(s2div2);
+            section2.appendChild(s2div3);
         });
 }
 
